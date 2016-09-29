e7319ebe399f4584dda401bf776485f236e5f1d0
Atef Haque <atefth@gmail.com>
2016-09-29 20:13:25 +0600

    Completed 3 directives

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,8 @@ var livereload = require('gulp-livereload');
 var connect = require('connect');
 var lrc = require('connect-livereload');
 var serveStatic = require('serve-static');
+var templateCache = require('gulp-angular-templatecache');
+var clean = require('gulp-clean');
 
 var WEB_PORT = 9000;
 var APP_DIR = './';
@@ -25,20 +27,34 @@ gulp.task('http-server', function() {
     .listen(WEB_PORT);
 });
 
+gulp.task('clean', function () {
+    return gulp.src(APP_DIR+'scripts/templates.js')
+    .pipe(clean({force: true}))
+    .pipe(gulp.dest(APP_DIR+'scripts'));
+})
+
+gulp.task('templates', function () {
+   return gulp.src('templates/**/*.html')
+    .pipe(templateCache({'module': 'ngSimple'}))
+    .pipe(gulp.dest(APP_DIR+'scripts'));
+});
+
 // start local http server with watch and livereload set up
 gulp.task('server', function() {
     gulp.run('lr-server');
 
     var watchFiles = ['templates/*.html', 'scripts/*.js'];
-    gulp.watch(watchFiles, function(e) {
+    gulp.watch(watchFiles, ['clean', 'templates'], function() {
         console.log('Files changed. Reloading...');
-        gulp.src(watchFiles)
+        gulp
+        .src(watchFiles)
+        // .pipe(gulp.task('templates'))
         .pipe(livereload(lrs));
     });
 
     gulp.run('http-server');
 });
 
-gulp.task('default', function() {
-    gulp.run('server');
+gulp.task('default', ['server'], function () {
+    
 });
--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 angular
-.module('demo', ['ngSimple'])
+.module('demo', ['ngSimple', 'ui.bootstrap', 'ngSanitize'])
 .controller('DemoCtrl', ['$scope', function ($scope) {
 
 	// multiselect dropdown
+	$scope.options = [
+		{label: 'Not Multiselectable', value: 1, multiselectable: false},
+		{label: 'Multiselectable Option 1', value: 2},
+		{label: 'Multiselectable Option 2', value: 3},
+		{label: 'Multiselectable Option 3', value: 4}
+	];
 	$scope.filtered_status = [];
 	$scope.updateStatuses = function () {
 		console.log('ctrl', $scope.filtered_status);
 	}
-	
+
 	// draggable toggle
 	$scope.toggleModel = false;
+	$scope.onColor = '#2196F3';
+	$scope.offColor = '#848484';
 	$scope.doSomething = function () {
 		console.log('ctrl', $scope.toggleModel);
 	}
@@ -17,9 +25,23 @@ angular
 	// dual progress bar
 	$scope.min = 0;
 	$scope.max = 60;
-	$scope.status = 'initiated';
+	$scope.status = '1';
 	$scope.score = 0;
-	$scope.statuses = [{label: 'Initiated', value: 'initiated', order: 0, color: 'gray'}, {label: 'Validated', value: 'validated', order: 1, color: '#D91'}, {label: 'Approved', value: 'approved', order: 2, color: '#4cae4c'}];
-	$scope.extraStatuses = [{label: 'Re-Validation Required', value: 'revalidation_required', order: 3, color: 'red'}]
+	$scope.statuses = [
+		{label: 'State 1', value: '1', order: 0, color: '#848484'},
+		{label: 'State 2', value: '2', order: 1, color: '#d19a3a'},
+		{label: 'State 3', value: '3', order: 2, color: '#4cae4c'},
+		{label: 'State 4', value: '4', order: 3, color: '#fcff48'}
+	];
+	$scope.extraStatuses = [
+		{label: 'State 5', value: '5', order: 4, color: '#e33c34'},
+		{label: 'State 6', value: '6', order: 5, color: '#d45cff'}
+	];
+	$scope.setStatus = function (status) {
+		$scope.status = status.value;
+	};
+	$scope.setExtraStatus = function (status) {
+		$scope.status = status.value;
+	};
 
 }])
